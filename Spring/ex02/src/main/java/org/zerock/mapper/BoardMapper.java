a875abeaec1993cdff002376c9fbd42bcb0932a6
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-09-15 14:47:40 +0900

    Spring checkpoint

---
+++ Spring/ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,21 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+
+	public void insert(BoardVO board);
+
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
+	
+}
---
+++ Spring/ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,51 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	@Override
+	public void register(BoardVO board) {
+		// TODO Auto-generated method stub
+		log.info("register......" + board);
+
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		// TODO Auto-generated method stub
+		return mapper.getList();
+	}
+}
