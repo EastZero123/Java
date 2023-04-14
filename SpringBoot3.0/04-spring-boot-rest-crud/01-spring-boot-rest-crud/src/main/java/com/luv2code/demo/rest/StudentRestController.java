2cfcf753f12924c411a15e85ca26380e3e2fa61c
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2023-04-14 16:54:47 +0900

    CRUD checkpoint

---
+++ SpringBoot3.0/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,33 @@
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData() {
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+
+        return theStudents;
+    }
+}
