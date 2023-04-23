59e71535a85f13276c72c0ec5b5665331cb53389
EastZero123 <cdy1005p@naver.com>
2023-04-23 20:51:09 +0900

    JPA

--- SpringBoot3.0/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ SpringBoot3.0/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.luv2code.springboot.cruddemo.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
